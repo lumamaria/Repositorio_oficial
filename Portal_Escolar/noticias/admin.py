c262fe196928e9637067577cc12752f2f9b794b8
Luma Maria Andrade Bandeira <luma.maria@escolar.ifrn.edu.br>
2026-09-09 17:32:15 -0300

    Provável parte final

--- Portal_Escolar/noticias/admin.py
+++ Portal_Escolar/noticias/admin.py
@@ -1,8 +1,26 @@
 from django.contrib import admin
 from .models import Categoria, Tag, Noticia, Perfil
-# Register your models here.
 
-admin.site.register(Categoria)
-admin.site.register(Tag)
-admin.site.register(Noticia)
-admin.site.register(Perfil)
+
+class CategoriaAdmin(admin.ModelAdmin):
+    list_display = ('nome',)
+    search_fields = ('nome',)
+
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('titulo',)
+    search_fields = ('titulo',)
+
+class NoticiaAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'categoria')
+    search_fields = ('titulo', 'texto')
+    list_filter = ('categoria',)
+
+class PerfilAdmin(admin.ModelAdmin):
+    list_display = ('usuario', 'bio')
+    search_fields = ('usuario__username',)
+
+
+admin.site.register(Categoria, CategoriaAdmin)
+admin.site.register(Tag, TagAdmin)
+admin.site.register(Noticia, NoticiaAdmin)
+admin.site.register(Perfil, PerfilAdmin)
